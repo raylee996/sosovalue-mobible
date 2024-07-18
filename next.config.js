fc9f6712e557e249d15d64d9db0fbfb931c00b01
DESKTOP-1E1MPL0\zj <weifan.li@imaginelabs.space>
2024-07-18 14:49:21 +0800

    test3

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const { i18n } = require("./next-i18next.config.js");
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-  register: false
+  reloadOnOnline: false
 });
 
 // ANALYZE=true npm run build
