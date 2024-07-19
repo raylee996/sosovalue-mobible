e64cfa6e5ee6274d2d953024badaba2f49de2b63
DESKTOP-1E1MPL0\zj <weifan.li@imaginelabs.space>
2024-07-19 18:29:18 +0800

    test24

--- next.config.js
+++ next.config.js
@@ -11,6 +11,13 @@ const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   reloadOnOnline: false,
+  fallbacks: {
+    document: '/_offline',
+    audio: '',
+    font: '',
+    image: '',
+    video: ''
+  },
   additionalManifestEntries: [
     {url: '/_offline', revision: null},
   ]
