d17abb6589d357dd507c15361630d4f8991ff2b9
liweifan <weifan.li@imaginelabs.space>
2024-07-21 15:37:18 +0800

    ass9

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -11,6 +11,7 @@ const CustomErrorComponent = ({ statusCode }: ErrorProps) => {
 
   if (!navigator?.onLine) {
     router.push('/_offline');
+    return;
   }
 
   return <Error statusCode={statusCode} />;
